9a02579ef7910db915e9c924a8c64b0a2834c6fd
Dalisson Figueiredo <dalissonfigueiredo@gmail.com>
2020-09-15 23:02:17 -0300

    Update si_snr_loss.py

--- si_snr_loss.py
+++ si_snr_loss.py
@@ -5,29 +5,44 @@ def upit_loss_2speakers(y_hat, y):
     the si_snr loss computed for two speakers
     y_hat: tensor shaped like (2, T)
     y: tensor shape like (2, T)
-
     where T = utterance duration
     '''
-    y_power = torch.pow(y, 2).sum(-1, keepdim=False)
+    si_til_numerator = (y@y_hat.t()).t()[..., None] * y
+    si_til_denominator = (y * y).sum(-1).sqrt().unsqueeze(-1)
+    si_til = si_til_numerator/ si_til_denominator
+    
+    #y[0]@y_hat[0]/mod(y_0)
+    #y[0]@y_hat[1]/mod(y_0)
+    #y[1]@y_hat[0]/mod(y_1)
+    #y[1]@y_hat[1]/mod(y_1)
+    si_til = torch.transpose(si_til, 0, 1)
+    
+    # y_hat[0] - y[0]@y_hat[0]/mod(y_0)
+    # y_hat[1] - y[0]@y_hat[1]/mod(y_0)
+    # y_hat[0] - y[1]@y_hat[0]/mod(y_1)
+    # y_hat[1] - y[1]@y_hat[1]/mod(y_1)
+    
+    e_til = y_hat - si_til
+    
+    #[[y_hat[0] - y[0]@y_hat[0]/mod(y_0), y_hat[1] - y[0]@y_hat[1]/mod(y_0)]
+    # y_hat[0] - y[1]@y_hat[0]/mod(y_1), y_hat[1] - y[1]@y_hat[1]/mod(y_1)]
+    
+    e_til_mod = (e_til * e_til).sum(-1).sqrt()
+    
+    #[[y[0]@y_hat[0]/mod(y_0), y[0]@y_hat[1]/mod(y_0)]
+    # y[1]@y_hat[0]/mod(y_1),  y[1]@y_hat[1]/mod(y_1)]
+    si_til_mod = (si_til * si_til).sum(-1).sqrt()
 
-    scale_factor = y_hat@y.t()/y_power
+    loss = 10*(torch.log10(si_til_mod) - torch.log10(e_til_mod))
+    
+    loss_one = loss[0][0] + loss[1][1]
+    loss_two = loss[1][0] + loss[0][1]
 
-    s_target = (torch.pow(y.unsqueeze(1) * scale_factor.t().unsqueeze(-1), 2).sum(-1)).t()
-
-    #e_noise
-    residual = y_hat.unsqueeze(1) - y
-    residual_norms = torch.pow(residual, 2).sum(-1, keepdim=False)
-
-    temp = (10*(torch.log10(s_target) - torch.log10(residual_norms)))
-    loss_one = temp[0, 0] + temp[1, 1]
-    loss_two = temp[0, 1] + temp[1, 0]
-    maximum_loss = loss_one if loss_one > loss_two else loss_two
-
-    return -maximum_loss
+    return -max(loss_one, loss_two)
     
 def si_snr(y_hat, y):
     loss = 0
     for prediction in y_hat:
-        loss += si_snr_2speaker(prediction.squeeze(0), y)
+        loss += upit_loss_2speaker(prediction.squeeze(0), y)
     
     return loss
