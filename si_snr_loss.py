4094cbd9bbac5a5394982083b0faf4541c96eb45
Dalisson Figueiredo <dalissonfigueiredo@gmail.com>
2020-09-21 10:36:02 -0300

    Update si_snr_loss.py
    loss changed

--- si_snr_loss.py
+++ si_snr_loss.py
@@ -44,6 +44,8 @@ def upit_loss(y_hat, y):
     
 def si_snr(y_hat, y):
     loss = 0
+    counter = 0
     for prediction in y_hat:
         loss -= upit_loss(prediction.squeeze(0), y)
-    return loss
+        counter += 1
+    return loss/counter
