f3e4ca5ee1a8f5ad5a2ff83e805bcac350ec96aa
Dalisson Figueiredo <dalissonfigueiredo@gmail.com>
2020-09-21 10:26:56 -0300

    Update si_snr_loss.py
    loss dividing by c now

--- si_snr_loss.py
+++ si_snr_loss.py
@@ -32,20 +32,18 @@ def upit_loss(y_hat, y):
     #[[y[0]@y_hat[0]/mod(y_0), y[0]@y_hat[1]/mod(y_0)]
     # y[1]@y_hat[0]/mod(y_1),  y[1]@y_hat[1]/mod(y_1)]
     si_til_mod = (si_til * si_til).sum(-1)
-
+    n_speakers = loss.shape[0]
     loss = 10*(torch.log10(si_til_mod) - torch.log10(e_til_mod))
     
-    n_speakers = loss.shape[0]
+   
     
     indexes = torch.tensor(list(range(n_speakers)) +list(range(n_speakers))).unfold(0, n_speakers, 1)[:-1]
     
-    return torch.gather(loss, 0, indexes).sum(-1).max()
+    return torch.gather(loss, 0, indexes).sum(-1).max()/n_speakers
 
     
 def si_snr(y_hat, y):
     loss = 0
-    counter = 0
     for prediction in y_hat:
         loss -= upit_loss(prediction.squeeze(0), y)
-        counter += 1
-    return loss/counter
+    return loss
